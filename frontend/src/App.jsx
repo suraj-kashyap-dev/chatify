980ff2174fe4051ddee24aec6c169eea1700622d
surajkashyap <surajkashyapp9911@gmail.com>
2024-02-25 15:15:54 +0530

    add routes to frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,9 @@ import Login from './pages/accounts/Login';
 import Home from "./pages/Home";
 
 function App(params) {
-  const isAuth = false;
-
   return (
     <>
-      {isAuth ? <Home></Home> : <Login></Login>}
+      <Home></Home>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./pages/accounts/Login.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
